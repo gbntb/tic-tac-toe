f5931b61f9e08f62ec1d2a63df8dfe1d24619e11
gbntb <228612152+gbntb@users.noreply.github.com>
2025-12-01 05:49:48 -0400

    Move Game players into an array

--- lib/game.rb
+++ lib/game.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
+require_relative 'player'
+require_relative 'board'
+
 # Main Game class. This is where it all comes together.
 class Game
-  attr_reader :player1, :player2, :board
+  attr_reader :players, :board
 
   def initialize
-    self.player1 = Player.new('Player 1', 'X')
-    self.player2 = Player.new('Player 2', 'O')
+    self.players = [Player.new('Player 1', 'X'), Player.new('Player 2', 'O')]
     self.board = Board.new
   end
 
   private
 
-  attr_writer :player1, :player2, :board
+  attr_writer :players, :board
 end
